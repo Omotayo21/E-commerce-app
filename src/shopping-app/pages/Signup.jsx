c2c2847d17cf4dcde49e26a10c8a7244f222c4e6
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2024-04-01 15:35:40 +0100

    Update Signup.jsx

--- src/shopping-app/pages/Signup.jsx
+++ src/shopping-app/pages/Signup.jsx
@@ -184,7 +184,7 @@ const [isLoading, setIsLoading] = useState(false);
                 </div>
                 <button
                   type="submit"
-                  className={`w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  className={`w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
                   onClick={signUp}
                 >
                     {isLoading ? 'Processing...' : 'Sign Up'}
