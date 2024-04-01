35b5c43b6214fb1b9b64e0d851519472ec760810
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2024-04-01 15:24:24 +0100

    Update Login.jsx

--- src/shopping-app/pages/Login.jsx
+++ src/shopping-app/pages/Login.jsx
@@ -18,7 +18,8 @@ function Login() {
   const [notification, setNotification] = useState(null);
   const [validationErrors, setValidationErrors] = useState({});
   const [passwordVisible, setPasswordVisible] = useState(false);
-  //when you come back do login in with google
+  const [isLoading, setIsLoading] = useState(false);
+  
   const validateForm = () => {
     const errors = {};
 
@@ -47,6 +48,7 @@ function Login() {
         await signInWithEmailAndPassword(authentication, email, password);
         const user = authentication.currentUser;
         //dispatch(userLoggedIn(user));
+        setIsLoading(true);
         const userUid = user.uid;
 
         const response = await fetch(
@@ -212,7 +214,7 @@ function Login() {
                   type="submit"
                   className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
-                  Sign in
+                     {isLoading ? 'Processing...' : 'Sign in'}
                 </button>
                 <button
                   className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm pl-16 py-2.5 text-center flex flex-row"
